ec401e8e840e059bf299c61562865951371bcd60
Joseph Allen Jemera <jajemera@hotmail.com>
2018-09-21 11:45:25 -0700

    Added fake jms property file (POC for multiple property files)

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -5,6 +5,7 @@ import guru.springframework.controllers.MyController;
 import guru.springframework.controllers.PropertyInjectedController;
 import guru.springframework.controllers.SetterInjectedController;
 import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -19,6 +20,8 @@ public class DiDemoApplication {
 
 		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 
+		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+
 		System.out.println(controller.hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
@@ -26,5 +29,7 @@ public class DiDemoApplication {
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 
 		System.out.println("DataSource username: " + fakeDataSource.getUser());
+
+		System.out.println("JMS Username: " + fakeJmsBroker.getUser());
 	}
 }
