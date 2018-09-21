334e27043b92dcee688ce315f5eac8f16db9eb61
Joseph Allen Jemera <jajemera@hotmail.com>
2018-09-20 22:09:46 -0700

    Initial Checkin

---
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -0,0 +1,24 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+@Controller
+public class ConstructorInjectedController {
+
+    private GreetingService greetingService;
+
+    // You can annotate this as autowired just to specify your intent, but it is completely optional. DI will still occur.
+
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
